80f99b5996c6e951943d9f6e3f3559af667abff0
fiol877 <fiol877@DESKTOP-FOKNO27>
2018-10-03 17:30:16 +0200

    Class bernat creat

---
+++ src/practiagitgrup/Bernat.java
@@ -0,0 +1,14 @@
+package practiagitgrup;
+
+public class Bernat {
+    private String nom;
+    private int edat;
+    public static void main(String[] args) {
+        
+    }
+    
+    public void Bernat(String nom, int edat){
+        this.nom = nom;
+        this.edat = edat;
+    }
+}
