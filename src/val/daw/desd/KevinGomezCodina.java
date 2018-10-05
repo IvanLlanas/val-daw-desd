c742854c23ca54057236a2c5dec668c26d4aa3c1
Kevin <kevin.gomez.codina@gmail.com>
2018-10-05 18:19:44 +0200

    Clase de Kevin Gómez Codina.

---
+++ src/val/daw/desd/KevinGomezCodina.java
@@ -0,0 +1,21 @@
+package val.daw.desd;
+
+public class KevinGomezCodina {
+	private String nombre;
+	
+	public KevinGomezCodina() {
+		nombre = "";
+	}
+	
+	public KevinGomezCodina(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+}
