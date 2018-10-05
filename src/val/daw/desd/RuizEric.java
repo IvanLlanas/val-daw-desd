63db84045e5060468bbbc1861656a6e7bf83c82f
stark <stark@DESKTOP-KGMMH3C>
2018-10-05 18:25:50 +0200

    Nuevo

---
+++ src/val/daw/desd/RuizEric.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package val.daw.desd;
+
+
+import java.util.Scanner;
+
+public class RuizEric {
+ public static void main(String[] args) {
+    
+    Scanner in = new Scanner(System.in);
+ }
+    public static void Eric (){
+
+    System.out.println("Aquest mètode es fals");
+
+    System.out.print ("Quiero crear conflicto :) ");
+
+    
+
+}
+
+    public static void Modificado (){
+
+    System.out.println("Aquest mètode es fals");
+
+    
+
+}
+}
+
