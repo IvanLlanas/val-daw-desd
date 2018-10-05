4b139a01784ad77a7269f6ecf39a7d31f1cec827
DESKTOP-VQE5HBA\Madao <pau.lopez56@gmail.com>
2018-10-05 18:20:28 +0200

    Clase LopezPau.

---
+++ src/val/daw/desd/LopezPau.java
@@ -0,0 +1,17 @@
+package val.daw.desd;
+
+
+public class LopezPau {
+    int num1 = 3;
+    int num2 = 2;
+
+    public LopezPau(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    
+    public int Suma(){
+        return num1+num2;
+    }
+    
+}
