15c01784dcc45c5de8e774118c2ede86016b59e1
AMX <AMX@AMX>
2018-10-05 19:21:31 +0200

    inici programa Max Freixa

---
+++ src/val/daw/desd/FreixaMax.java
@@ -0,0 +1,18 @@
+package val.daw.desd;
+
+/**
+ *
+ * @author AMX
+ */
+
+import java.util.Scanner;
+
+public class FreixaMax {
+
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        System.out.print("");
+    }
+}
