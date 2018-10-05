d183a7b64bf4cf08f860217ebecd95db14567a28
Ivan <Ivan@NC>
2018-10-05 16:17:00 +0200

    Commit inicial

---
+++ src/val/daw/desd/ValDawDesd.java
@@ -0,0 +1,68 @@
+package val.daw.desd;
+
+/**
+ *
+ * @author Ivan Llanas
+ */
+public class ValDawDesd
+{
+    /*
+       Empleneu el cos de la "vostra" funció amb algun codi interessant i
+       actualitzeu el repositori remot.
+    */
+    public static void main(String[] args)
+    {
+        System.out.println ("Jelou, guorl!");
+    }
+
+    public String getAlbaAlex ()
+    {}
+    
+    public String getBordesFelix ()
+    {}
+
+    public String getBrugueraAbraham ()
+    {}
+
+    public String getDuranCristian ()
+    {}
+    
+    public String getFiolBernat ()
+    {}
+    
+    public String getFreixaMax ()
+    {}
+    
+    public String getGarciaMario ()
+    {}
+    
+    public String getGomezKevin ()
+    {}
+    
+    public String getLopezPau ()
+    {}
+    
+    public String getLorenteLuis ()
+    {}
+    
+    public String getMaciasAlejandro ()
+    {}
+    
+    public String getMinanaPol ()
+    {}
+    
+    public String getOrtizDavid ()
+    {}
+    
+    public String getOteroDavid ()
+    {}
+    
+    public String getReyesRoger ()
+    {}
+    
+    public String getRodriguezDavid ()
+    {}
+    
+    public String getRuizEric ()
+    {}
+}
